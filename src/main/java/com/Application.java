cb7067fb04fcdb1934135cb9e04fe8f0d18f60c1
ritik <jainritik153@gmail.com>
2020-07-28 13:50:16 +0530

    by me commit

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	//comment 
+	//comment by me
 	
 	public static void main(String args[]){
 		SpringApplication.run(Application.class,args);
